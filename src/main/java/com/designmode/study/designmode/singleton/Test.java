6f9aca02130151810eb9f9292f1e600a89f817b6
chenlinks <chenlingys@live.com>
2019-02-26 00:34:20 +0800

    测试webhook

--- src/main/java/com/designmode/study/designmode/singleton/Test.java
+++ src/main/java/com/designmode/study/designmode/singleton/Test.java
@@ -11,6 +11,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("陈领");
         System.out.println("chenling");
         System.out.println("chenling");
     }
