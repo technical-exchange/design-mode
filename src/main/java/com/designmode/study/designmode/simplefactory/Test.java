e5791f99d8c84f468d0adf42c568cf67e89517ea
chenlinks <chenlingys@live.com>
2019-02-25 23:31:28 +0800

    commit

---
+++ src/main/java/com/designmode/study/designmode/simplefactory/Test.java
@@ -0,0 +1,15 @@
+package com.designmode.study.designmode.simplefactory;
+
+/***
+ * @ClassName: Test
+ * @Description:TODO
+ * @author: chenling
+ * @Date: 2019/2/25 23:29
+ * @version : V1.0.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
